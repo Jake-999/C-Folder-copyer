bf130510d0c662745c62df9becae8177cca286fd
jakeElliott <45903725+Jake-999@users.noreply.github.com>
2021-04-12 19:28:29 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PhoneCopyer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Folder1loc = new System.Windows.Forms.Button();
+            this.Folder2loc = new System.Windows.Forms.Button();
+            this.copyiedfiles = new System.Windows.Forms.RichTextBox();
+            this.Movebtn = new System.Windows.Forms.Button();
+            this.Copybtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Folder1loc
+            // 
+            this.Folder1loc.Location = new System.Drawing.Point(44, 25);
+            this.Folder1loc.Name = "Folder1loc";
+            this.Folder1loc.Size = new System.Drawing.Size(166, 53);
+            this.Folder1loc.TabIndex = 0;
+            this.Folder1loc.Text = "Intial directory";
+            this.Folder1loc.UseVisualStyleBackColor = true;
+            this.Folder1loc.Click += new System.EventHandler(this.phoneloc_Click);
+            // 
+            // Folder2loc
+            // 
+            this.Folder2loc.Location = new System.Drawing.Point(298, 25);
+            this.Folder2loc.Name = "Folder2loc";
+            this.Folder2loc.Size = new System.Drawing.Size(168, 53);
+            this.Folder2loc.TabIndex = 1;
+            this.Folder2loc.Text = "Target directory";
+            this.Folder2loc.UseVisualStyleBackColor = true;
+            this.Folder2loc.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // copyiedfiles
+            // 
+            this.copyiedfiles.Location = new System.Drawing.Point(44, 148);
+            this.copyiedfiles.Name = "copyiedfiles";
+            this.copyiedfiles.Size = new System.Drawing.Size(422, 301);
+            this.copyiedfiles.TabIndex = 2;
+            this.copyiedfiles.Text = "";
+            // 
+            // Movebtn
+            // 
+            this.Movebtn.Location = new System.Drawing.Point(43, 502);
+            this.Movebtn.Name = "Movebtn";
+            this.Movebtn.Size = new System.Drawing.Size(167, 51);
+            this.Movebtn.TabIndex = 3;
+            this.Movebtn.Text = "Move";
+            this.Movebtn.UseVisualStyleBackColor = true;
+            this.Movebtn.Click += new System.EventHandler(this.Movebtn_Click);
+            // 
+            // Copybtn
+            // 
+            this.Copybtn.Location = new System.Drawing.Point(321, 502);
+            this.Copybtn.Name = "Copybtn";
+            this.Copybtn.Size = new System.Drawing.Size(145, 51);
+            this.Copybtn.TabIndex = 4;
+            this.Copybtn.Text = "Copy";
+            this.Copybtn.UseVisualStyleBackColor = true;
+            this.Copybtn.Click += new System.EventHandler(this.Copybtn_Click);
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Location = new System.Drawing.Point(201, 585);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(118, 60);
+            this.deletebtn.TabIndex = 5;
+            this.deletebtn.Text = "Delete";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 691);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.Copybtn);
+            this.Controls.Add(this.Movebtn);
+            this.Controls.Add(this.copyiedfiles);
+            this.Controls.Add(this.Folder2loc);
+            this.Controls.Add(this.Folder1loc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Folder1loc;
+        private System.Windows.Forms.Button Folder2loc;
+        private System.Windows.Forms.Button Movebtn;
+        private System.Windows.Forms.Button Copybtn;
+        protected internal System.Windows.Forms.RichTextBox copyiedfiles;
+        private System.Windows.Forms.Button deletebtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PhoneCopyer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // setting the paths to null 
+        private static string folder2_loc = "";
+        private static string folder1_loc = "";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Folder2_location();
+        }
+        private void phoneloc_Click(object sender, EventArgs e)
+        {
+            folder1_Location();
+        }
+        private void Movebtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Copybtn_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo diSource = new DirectoryInfo(folder1_loc);
+            DirectoryInfo diTarget = new DirectoryInfo(folder2_loc);
+            Folder_copy(diSource, diTarget);
+        }
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            string message = "Are you sure you want to delete this directory";
+            string caption = "";
+            // Displays the MessageBox.
+            DirectoryInfo diSource = new DirectoryInfo(folder1_loc);
+
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                Folder_delete(diSource);
+            }
+        }
+
+        // 
+        private static void folder1_Location()
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    folder1_loc = dialog.SelectedPath;
+                }
+            }
+        }
+        private static void Folder2_location()
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    folder2_loc = dialog.SelectedPath;
+                }
+            }
+
+        }
+
+
+        private void Folder_copy(DirectoryInfo diSource, DirectoryInfo diTarget)
+        {
+
+            Directory.CreateDirectory(diTarget.FullName);
+
+            foreach (FileInfo fi in diSource.GetFiles())
+            {
+
+                copyiedfiles.AppendText($"Copying {diTarget.FullName}{fi.Name} \n");
+                fi.CopyTo(Path.Combine(diTarget.FullName, fi.Name), true);
+            }
+            foreach (DirectoryInfo diSourceSubDir in diSource.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    diTarget.CreateSubdirectory(diSourceSubDir.Name);
+                Folder_copy(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+        private void Folder_delete(DirectoryInfo source)
+        {
+            copyiedfiles.Text = "";
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                File.Delete(fi.FullName);
+                copyiedfiles.AppendText($"Deleting {source.FullName}{fi.Name}\n");
+
+            }
+            foreach (DirectoryInfo sourecdi in source.GetDirectories())
+            {
+                sourecdi.Delete(true);
+                copyiedfiles.AppendText($"Deleting {source.FullName}\n");
+            }
+        }
+
+    }
+
+}
+
